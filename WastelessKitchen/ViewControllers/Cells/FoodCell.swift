d7229f99edbb3d22d47d859d8e5f5e24fd985e9f
Adam <adamjackson@pursuit.org>
2019-12-18 14:52:30 -0500

    Added placeholder files

---
+++ WastelessKitchen/ViewControllers/Cells/FoodCell.swift
@@ -0,0 +1,13 @@
+//
+//  FoodCell.swift
+//  WastelessKitchen
+//
+//  Created by God on 12/18/19.
+//  Copyright © 2019 God. All rights reserved.
+//
+
+import UIKit
+
+class FoodCell: UICollectionViewCell {
+    
+}
