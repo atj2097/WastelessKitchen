fcc5c3a4cc06f30135b3bb0c15159cb6abd0d93d
Adam <adamjackson@pursuit.org>
2019-12-18 14:47:47 -0500

    Added Tab Bar folder

---
+++ WastelessKitchen/ViewControllers/TabControllers/InputVC.swift
@@ -0,0 +1,34 @@
+//
+//  InputVC.swift
+//  WastelessKitchen
+//
+//  Created by God on 12/18/19.
+//  Copyright © 2019 God. All rights reserved.
+//
+
+import UIKit
+class InputVC: UIViewController {
+    //Views
+    lazy var datePicker: UIDatePicker = {
+        let picker = UIDatePicker()
+        return picker
+    }()
+    lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView()
+        return cv
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+//    
+//    private func 
+//    
+    
+    
+    }
+
+
+
