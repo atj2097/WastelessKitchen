1700ddab32405c84b37cdbca55b27b03db2fcb88
Ian <iancervone@pursuit.org>
2019-12-18 16:48:06 -0500

    added firebase services, and appUser Model

---
+++ WastelessKitchen/FirebaseHelpers/FirestoreService.swift
@@ -0,0 +1,64 @@
+import Foundation
+import FirebaseFirestore
+
+fileprivate enum FireStoreCollections: String {
+    case users
+    case posts
+}
+
+enum SortingCriteria: String {
+    case fromNewestToOldest = "dateCreated"
+    var shouldSortDescending: Bool {
+        switch self {
+        case .fromNewestToOldest:
+            return true
+        }
+    }
+}
+
+
+class FirestoreService {
+    static let manager = FirestoreService()
+    
+    private let db = Firestore.firestore()
+    
+    //MARK: AppUsers
+    func createAppUser(user: AppUser, completion: @escaping (Result<(), Error>) -> ()) {
+        var fields = user.fieldsDict
+        fields["dateCreated"] = Date()
+        db.collection(FireStoreCollections.users.rawValue).document(user.uid).setData(fields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+                print(error)
+            }
+            completion(.success(()))
+        }
+    }
+  
+    
+    func updateCurrentUser(userName: String? = nil, photoURL: URL? = nil, completion: @escaping (Result<(), Error>) -> ()){
+        guard let userId = FirebaseAuthService.manager.currentUser?.uid else {
+            //MARK: TODO - handle can't get current user
+            return
+        }
+        var updateFields = [String:Any]()
+        
+        if let user = userName {
+            updateFields["userName"] = user
+        }
+        
+        if let photo = photoURL {
+            updateFields["photoURL"] = photo.absoluteString
+        }
+        db.collection(FireStoreCollections.users.rawValue).document(userId).updateData(updateFields) { (error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+
+
+    private init () {}
+}
